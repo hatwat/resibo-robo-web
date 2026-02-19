ea5978d85f72d199901b498a44ff17d05a26b78b
atojimenez <hatwat93@gmail.com>
2026-02-19 20:06:35 +0800

    fix: move @import before @tailwind in index.css

--- src/components/PendingInvoiceCard.jsx
+++ src/components/PendingInvoiceCard.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabaseClient'
 import LoginForm from './components/LoginForm'
 import PendingInvoicesGrid from './components/PendingInvoicesGrid'
 
